b12f1c5f78c231ba492acc7b3301b59cffbd1a9c
celio_henrique <celio_henrique@10.1.49.193>
2013-08-02 16:35:38 -0300

    Na classe Marca, foram adicionados um Construtor e Metodos getter e set

---
+++ CrudJDBC/src/br/com/modelo/negocio/Marca.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.modelo.negocio;
+
+/**
+ *
+ * @author celio_henrique
+ */
+public class Marca {
+    
+    private int id;
+    private String nome;
+
+    public Marca() {
+       
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
